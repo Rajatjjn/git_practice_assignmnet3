5455af58517baa5450655596551f273f8b2855ea
rajatjjn <rajatjjn0510@gmail.com>
2022-09-20 18:26:49 +0530

    code fixed by m1

--- prime.js
+++ prime.js
@@ -1,16 +1,17 @@
 //Problem 1: Create a function to check if a number is Prime or Not
 function checkprime(num){
-    let factors=5;
+    let factors=0;
     for(i=0;i<=num;i++){
       if(num%i==0){
-        factors;
+        factors++;
+        
       }
     }
     if(factors==2){
       console.log(num,"is prime");
       
     }else {
-      console.(num,"not prime");
+      console.log(num,"not prime");
     }
     }
     checkprime(38);
