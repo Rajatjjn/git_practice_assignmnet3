f9ba4c3f00717641cfa01eba3e1068d1611d173b
eucap <surya.promi@gmail.com>
2022-09-20 18:28:33 +0530

    code fixed by eucap

--- prime.js
+++ prime.js
@@ -1,16 +1,16 @@
 //Problem 1: Create a function to check if a number is Prime or Not
 function checkprime(num){
-    let factors=5;
+    let number=0;
     for(i=0;i<=num;i++){
       if(num%i==0){
-        factors;
+        number++;
       }
     }
-    if(factors==2){
+    if(number==2){
       console.log(num,"is prime");
       
     }else {
-      console.(num,"not prime");
+      console.log(num,"not prime");
     }
     }
     checkprime(38);
